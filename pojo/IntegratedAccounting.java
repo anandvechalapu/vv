9fc8895ef987fb355b17a50ff271c719c2c80b1c
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-21 12:41:59 +0530

    initial commit

---
+++ pojo/IntegratedAccounting.java
@@ -0,0 +1,146 @@
+public class IntegratedAccounting {
+ 
+    private String depositAccount;
+    private String premiumAdjustments;
+    private String premiumReversalAdjustments;
+    private String gstPayments;
+    private String invoice;
+    private String tds;
+    private String charges;
+    private String stampDuty;
+    private String commissionCharges;
+    private String voucherPayment;
+    private String neftTransfer;
+    private String neftRefunds;
+    private String interestCalculations;
+    private String credit;
+    private String fundManagementCharges;
+    private String policyAdministrationCharges;
+    private String penaltiesLatePaymentFees;
+    private String surrenderCharges;
+    private String withdrawalChargesAtMPH;
+    private String claimBooking;
+    private String claimPayment;
+    private String claimRepudiation;
+    private String transferPolicyInOut;
+    private String conversion;
+    private String gratuityLeaveEncashment;
+ 
+    public String getDepositAccount() {
+        return depositAccount;
+    }
+ 
+    public void setDepositAccount(String depositAccount) {
+        this.depositAccount = depositAccount;
+    }
+ 
+    public String getPremiumAdjustments() {
+        return premiumAdjustments;
+    }
+ 
+    public void setPremiumAdjustments(String premiumAdjustments) {
+        this.premiumAdjustments = premiumAdjustments;
+    }
+ 
+    public String getPremiumReversalAdjustments() {
+        return premiumReversalAdjustments;
+    }
+ 
+    public void setPremiumReversalAdjustments(String premiumReversalAdjustments) {
+        this.premiumReversalAdjustments = premiumReversalAdjustments;
+    }
+ 
+    public String getGstPayments() {
+        return gstPayments;
+    }
+ 
+    public void setGstPayments(String gstPayments) {
+        this.gstPayments = gstPayments;
+    }
+ 
+    public String getInvoice() {
+        return invoice;
+    }
+ 
+    public void setInvoice(String invoice) {
+        this.invoice = invoice;
+    }
+ 
+    public String getTds() {
+        return tds;
+    }
+ 
+    public void setTds(String tds) {
+        this.tds = tds;
+    }
+ 
+    public String getCharges() {
+        return charges;
+    }
+ 
+    public void setCharges(String charges) {
+        this.charges = charges;
+    }
+ 
+    public String getStampDuty() {
+        return stampDuty;
+    }
+ 
+    public void setStampDuty(String stampDuty) {
+        this.stampDuty = stampDuty;
+    }
+ 
+    public String getCommissionCharges() {
+        return commissionCharges;
+    }
+ 
+    public void setCommissionCharges(String commissionCharges) {
+        this.commissionCharges = commissionCharges;
+    }
+ 
+    public String getVoucherPayment() {
+        return voucherPayment;
+    }
+ 
+    public void setVoucherPayment(String voucherPayment) {
+        this.voucherPayment = voucherPayment;
+    }
+ 
+    public String getNeftTransfer() {
+        return neftTransfer;
+    }
+ 
+    public void setNeftTransfer(String neftTransfer) {
+        this.neftTransfer = neftTransfer;
+    }
+ 
+    public String getNeftRefunds() {
+        return neftRefunds;
+    }
+ 
+    public void setNeftRefunds(String neftRefunds) {
+        this.neftRefunds = neftRefunds;
+    }
+ 
+    public String getInterestCalculations() {
+        return interestCalculations;
+    }
+ 
+    public void setInterestCalculations(String interestCalculations) {
+        this.interestCalculations = interestCalculations;
+    }
+ 
+    public String getCredit() {
+        return credit;
+    }
+ 
+    public void setCredit(String credit) {
+        this.credit = credit;
+    }
+ 
+    public String getFundManagementCharges() {
+        return fundManagementCharges;
+    }
+ 
+    public void setFundManagementCharges(String fundManagementCharges) {
+        this.fundManagementCharges =
